bd1f19571e1f74b63d511f14a10053dc5cabcf09
mirzadele2-bit <mirz.adele2@gmail.com>
2026-07-04 01:53:00 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,13 @@
+
+const data = [
+  {
+    id: "test-1",
+    title: "סדרה לדוגמה",
+    category: "סדרות סיניות",
+    date: "2026-01-01",
+    description: "תקציר כאן",
+    episodes: [
+      { name: "פרק 1", link: "#" }
+    ]
+  }
+];
